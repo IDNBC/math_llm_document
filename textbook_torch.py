ae7aa910a611f4fa7b012b47a99f3c3e04e70519
Owner <iida.nobuxyz345@gmail.com>
2025-06-10 06:46:33 +0900

    6月10日自宅でのコミット

--- textbook_torch.py
+++ textbook_torch.py
@@ -24,9 +24,11 @@ attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
 print("Attention weights:", attn_weights_2_tmp)
 print("Sum:", attn_weights_2_tmp.sum())
 
+
 # ソフトマックス関数の基本実装
 def softmax_naive(x):
     return torch.exp(x) / torch.exp(x).sum(dim=0)
+
 
 attn_weights_2_naive = softmax_naive(attn_scores_2)
 print("Attention weights:", attn_weights_2_naive)
@@ -109,7 +111,8 @@ print("正規化後の重み:", attn_weights_2)
 context_vec_2 = attn_weights_2 @ values
 print(context_vec_2)
 
-class SelfAttention_v1(nn.module):
+
+class SelfAttention_v1(nn.Module):
 
     def __init__(self, d_in, d_out):
         super().__init__()
@@ -128,4 +131,32 @@ class SelfAttention_v1(nn.module):
         )
         context_vec = attn_weights @ values
         return context_vec
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+
+class SelfAttention_v2(nn.Module):
+    
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+    
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
 
